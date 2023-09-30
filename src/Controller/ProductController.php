0388642b4403fc6821730a9c4fad7624006dbc47
carteur007 <julienmarsellas@gmail.com>
2023-09-30 21:39:05 +0200

    Fin du developpement de la fonctionnalité d'upload de fichier mise à jour du ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Form\Product1Type;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FileUploadError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 #[Route('/product')]
 class ProductController extends AbstractController
